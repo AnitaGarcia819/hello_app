f1e4cd06c9ef237a51ba7eacb2472759f1d13ca0
Anita Garcia <anitagarcia819@gmail.com>
2018-10-01 00:42:11 -0700

    Added bonus user stories: all minus edit text

--- app/src/main/java/com/example/anitagarcia/hello_world/MainActivity.java
+++ app/src/main/java/com/example/anitagarcia/hello_world/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +19,32 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view){
                 Log.i("Anita", "Anita clicked this button");
                 ((TextView) findViewById(R.id.textView)).setTextColor(getResources().getColor(R.color.colorPrimary));
+            }
+        });
+
+        findViewById(R.id.text_button).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Log.i("Anita", "Anita clicked this button");
+                ((TextView) findViewById(R.id.textView)).setText("Android is Awesome!");
+            }
+        });
+
+        findViewById(R.id.background_button).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Log.i("Anita", "Anita clicked this button");
+                findViewById(R.id.rootView).setBackgroundColor(getResources().getColor(R.color.colorSecondary));
+            }
+        });
+
+        findViewById(R.id.default_button).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Log.i("Anita", "Anita clicked this button");
+                ((TextView) findViewById(R.id.textView)).setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+                findViewById(R.id.rootView).setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                ((TextView) findViewById(R.id.textView)).setText(R.string.greeting);
             }
         });
     }
