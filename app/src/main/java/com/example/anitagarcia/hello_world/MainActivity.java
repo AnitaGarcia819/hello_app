a9c36e392f5ab45695871747793a824e14744564
Anita Garcia <anitagarcia819@gmail.com>
2018-10-01 22:13:33 -0700

    Added last bonus feature

--- app/src/main/java/com/example/anitagarcia/hello_world/MainActivity.java
+++ app/src/main/java/com/example/anitagarcia/hello_world/MainActivity.java
@@ -2,8 +2,9 @@ package com.example.anitagarcia.hello_world;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Log.i("Anita", "Anita clicked this button");
                 ((TextView) findViewById(R.id.textView)).setTextColor(getResources().getColor(R.color.colorPrimary));
             }
         });
@@ -25,26 +25,29 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.text_button).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Log.i("Anita", "Anita clicked this button");
-                ((TextView) findViewById(R.id.textView)).setText("Android is Awesome!");
+               String newText = ((EditText) findViewById(R.id.editText)).getText().toString();
+               if(TextUtils.isEmpty(newText)){
+                   ((TextView) findViewById(R.id.textView)).setText("Hello from Anita");
+
+               }else {
+                   ((TextView) findViewById(R.id.textView)).setText(newText);
+               }
             }
         });
 
         findViewById(R.id.background_button).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Log.i("Anita", "Anita clicked this button");
                 findViewById(R.id.rootView).setBackgroundColor(getResources().getColor(R.color.colorSecondary));
             }
         });
 
-        findViewById(R.id.default_button).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.rootView).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Log.i("Anita", "Anita clicked this button");
                 ((TextView) findViewById(R.id.textView)).setTextColor(getResources().getColor(R.color.colorPrimaryDark));
                 findViewById(R.id.rootView).setBackgroundColor(getResources().getColor(R.color.colorAccent));
-                ((TextView) findViewById(R.id.textView)).setText(R.string.greeting);
+                ((TextView) findViewById(R.id.textView)).setText("Hello from Anita");
             }
         });
     }
